2e962c06993a31cfb8ba35d4087711ebcaee7b60
Shubhankar Raj <mritunjaymishra8953@gmail.com>
2026-08-11 21:51:11 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+           int st = 0 , end = s.length() - 1;
+        while( st < end){  
+     
+            // Skip non-alphanumeric characters from left
+            if (!isalnum(s[st])) {
+                st++;
+                continue;
+            }
+        if (!isalnum(s[end])) {
+                end--;
+                continue;
+            }
+            if(tolower(s[st]) != tolower(s[end])){
+              return false;
+            }
+            st++;
+            end--;
+        }
+      
+            return true;
+    }
+};
