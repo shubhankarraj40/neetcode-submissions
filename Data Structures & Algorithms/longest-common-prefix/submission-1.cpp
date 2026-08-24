55cfd08fdd13b4e2269305ca24d9dd19db5f3f1f
Shubhankar Raj <mritunjaymishra8953@gmail.com>
2026-08-24 15:25:13 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        string ans = "";
+
+        for (int i = 0; i < strs[0].size(); i++) {
+
+            char ch = strs[0][i];
+
+            for (int j = 1; j < n; j++) {
+
+                if (i >= strs[j].size() || strs[j][i] != ch) {
+                    return ans;
+                }
+            }
+
+            ans.push_back(ch);
+        }
+
+        return ans;
+    }
+};
